629f4f6cf3660d6befa4ed405d8a116cdfe67cbc
Saurabh Potdar <44492033+SaurabhPotdar@users.noreply.github.com>
2023-01-29 19:38:40 +0530

    cleanup

--- gateway/src/main/java/com/tce/GatewayApplication.java
+++ gateway/src/main/java/com/tce/GatewayApplication.java
@@ -1,21 +1,13 @@
 package com.tce;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
-	}
-
-	@Bean
-	public WebClient webClient(@Value("${server.url}") final String serverUrl) {
-		return WebClient.create(serverUrl);
 	}
 
 }
