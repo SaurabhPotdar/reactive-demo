4b14db98b8667edb61a2aafb62fa210a1c0c84ea
Saurabh Potdar <44492033+SaurabhPotdar@users.noreply.github.com>
2023-03-11 20:11:09 +0530

    b3 header propagation

--- reactive-client/src/main/java/com/tce/service/TestService.java
+++ reactive-client/src/main/java/com/tce/service/TestService.java
@@ -2,6 +2,7 @@ package com.tce.service;
 
 import com.tce.model.Employee;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,8 @@ public class TestService {
         log.info("Get employees");
         return webClient.get()
                 .uri(serverUrl + "/employees")
+                .header("X-B3-TraceId", MDC.get("traceId"))
+                .header("X-B3-SpanId", MDC.get("spanId"))
                 .retrieve()
                 .bodyToFlux(Employee.class)
                 .doOnComplete(() -> log.info("Received response from server"));
